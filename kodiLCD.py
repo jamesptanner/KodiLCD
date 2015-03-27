3f9f29189c24b9fc1e46c89acc0f567c736d58ea
James Tanner <jamesptanner@outlook.com>
2015-03-27 20:52:02 +0000

    added the json/http code. and the next/prev code (that doesnt work).

--- kodiLCD.py
+++ kodiLCD.py
@@ -1,10 +1,7 @@
 # {"jsonrpc": "2.0", "method": "Player.PlayPause", "params": { "playerid": 0 }, "id": 1}
-# {"jsonrpc": "2.0", "method": "Player.GoTo", "params": { "playerid": 0, "to": "next" }, "id": 1}
-# {"jsonrpc": "2.0", "method": "Player.GoTo", "params": { "playerid": 0, "to": "previous" }, "id": 1}
-# {"jsonrpc": "2.0", "method": "Player.GetActivePlayers", "id": 1}
 # {"jsonrpc": "2.0", "method": "Player.GetItem", "params": { "properties": ["title", "album", "artist", "duration"], "playerid": 0 }, "id": "AudioGetItem"}
 # {"jsonrpc": "2.0", "method": "Player.GetItem", "params": { "properties": ["title", "album", "artist", "season", "episode", "duration", "showtitle"], "playerid": 1 }, "id": "VideoGetItem"}
-import httplib
+import httplib, urllib
 import json
 import time
 import math
@@ -15,17 +12,51 @@ import Adafruit_CharLCD as LCD
 lcd = LCD.Adafruit_CharLCDPlate()
 lcd.set_color(1,0,0)
 
-http = httplib.HttpConnection("192.168.1.143",8080)
+http = httplib.HTTPConnection("192.168.1.143",8080)
 
+def postKodiCommand(postCmd):
+	print postCmd
+	postHeader = {'Content-Type':'application/json'}
+	http.request("POST", "/jsonrpc",postCmd,postHeader)
+	response = http.getresponse()
+	print response.status, response.reason
+	jsonResponse = response.read()
+	print jsonResponse
+	return json.loads(jsonResponse)
+
+def getActivePlayer():
+	# {"jsonrpc": "2.0", "method": "Player.GetActivePlayers", "id": 1}
+	# {"id":1,"jsonrpc":"2.0","result":[{"playerid":1,"type":"video"}]}
+	# {"id":1,"jsonrpc":"2.0","result":[]}
+	postCmd = '''{"jsonrpc": "2.0", "method": "Player.GetActivePlayers", "id": 1}'''
+	resp = postKodiCommand(postCmd)
+	players = resp['result']
+	if len(players)==0:
+		return None
+	else:
+		return players[0]
 
 def goToNext():
+	# {"jsonrpc": "2.0", "method": "Player.GoTo", "params": { "playerid": 0, "to": "next" }, "id": 1}
+	postCmd = '{{"jsonrpc": "2.0", "method": "Player.GoTo", "params": {{ "playerid": 1, "position": 1 }}, "id": 1}}'
 	lcd.clear()
 	lcd.message("GOTO NEXT")
+	player = getActivePlayer()
+	if player is not None:
+		print player
+		postCmd.format(**player)
+		postKodiCommand(postCmd)
 
 def goToPrev():
+	# {"jsonrpc": "2.0", "method": "Player.GoTo", "params": { "playerid": 0, "to": "previous" }, "id": 1}
+	postCmd = '{{"jsonrpc": "2.0", "method": "Player.GoTo", "params": {{ "playerid": 1, "position": 0 }}, "id": 1}}'
 	lcd.clear()
 	lcd.message("GOTO PREV")
-
+	player = getActivePlayer()
+	if player is not None:
+		print player
+		postCmd.format(**player)
+		postKodiCommand(postCmd)
 
 def playPause():
 	lcd.clear()
@@ -35,11 +66,11 @@ def null_func():
 	lcd.clear()
 	lcd.message("NULL")
 
-buttons = { LCD.SELECT : playPause,
-           LCD.LEFT : goToPrev,
-           LCD.UP : null_func,
-           LCD.DOWN : null_func,
-           LCD.RIGHT : goToNext }
+buttons = {LCD.SELECT : playPause,
+           LCD.LEFT   : goToPrev,
+           LCD.UP     : null_func,
+           LCD.DOWN   : null_func,
+           LCD.RIGHT  : goToNext }
 
 
 
@@ -50,7 +81,7 @@ def main():
 			if lcd.is_pressed(button):
 				buttons[button]()
 				while lcd.is_pressed(button):
-					time.sleep(.5)
+					pass
 
 if __name__ == '__main__':
 	main()
